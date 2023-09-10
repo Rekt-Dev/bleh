d4722de9128ce398c8629570653138c6215645f3
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2023-09-10 13:29:44 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import "./styles.css";
+import TodoList from "./TodoList";
+import React, { useState } from "react";
+import "./App.css";
+
+interface Task {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+export default function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTask, setNewTask] = useState<string>("");
+
+  const addTask = () => {
+    if (newTask.trim() === "") return;
+    const newTaskObject: Task = {
+      id: Date.now(),
+      text: newTask,
+      completed: false
+    };
+
+    setTasks([...tasks, newTaskObject]);
+
+    setNewTask("");
+  };
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
